67543276e3121aaf74f08728fe1148574266308d
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2023-10-19 23:01:59 +0300

    add reverse sort function

--- lw1/UserService.php
+++ lw1/UserService.php
@@ -2,19 +2,24 @@
 require_once 'User.php';
 
 class UserService {
-    public static function sortByUsername($users, $direction = 'asc') {
+    public static function sortByUsername($users, $direction) {
         usort($users, function($a, $b) use ($direction) {
             if ($direction === 'asc') {
                 return strcmp($a->username, $b->username);
+            }else if ($direction === 'desc') {
+                return strcmp($b->username, $a->username);
             }
         });
+        
         return $users;
     }
     
-    public static function sortByBirthday($users, $direction = 'asc') {
+    public static function sortByBirthday($users, $direction) {
         usort($users, function($a, $b) use ($direction) {
             if ($direction === 'asc') {
                 return $a->birthday <=> $b->birthday;
+            }else if ($direction === 'desc'){
+                return $b->birthday <=> $a->birthday;
             }
         });
         
